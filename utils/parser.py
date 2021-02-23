97941e8d208c420c9b97ac1d456e148c4208aafe
Nigel Bowden <wifinigel@gmail.com>
2021-02-23 09:30:39 +0000

    Add lazy parser support

--- utils/parser.py
+++ utils/parser.py
@@ -18,6 +18,64 @@ def verb_expander(cmd_verb):
     # no match, return False
     return False
 
+def lazy_parser(cmd_text, command_list):
+    """
+    Parser that will take a command snippet and try to match to the 
+    command list. This allows a user to enter abbreviated commands
+    and make a match, as long as that abbreviation is unique.
+
+    Example:
+        
+        "sh wl"
+    
+    becomes:
+
+        "show wlan"
+    """
+    # tokenize & extract verb
+    tokens = cmd_text.split()
+    verb = tokens[0]
+    args = tokens[1:]
+
+    # check verb for possible abbreviation expansion
+    if verb not in supported_verbs:
+        verb = verb_expander(verb)
+    
+    if not verb:
+        # no match, time to bail
+        return [ False, [] ]
+    
+    # concatenate the verb and remaining args and see if we can
+    # get a (unique) partial match on the resulting string
+    # e.g. show wla
+    partial_command = "{} {}".format(verb, args[0])
+    num_partial_cmd_tokens = len(partial_command.split())
+    
+    # check for partial command matches in command list
+    matched_commands = []
+
+    for command in command_list:
+        if partial_command in command:
+            matched_commands.append(command)
+    
+    if len(matched_commands) == 1:
+
+        matched_command = matched_commands[0]
+
+        # tokenize matched command
+        matched_cmd_tokens = matched_command.split()
+
+        # reconstruct command with underscores instead of
+        # spaces, ready for command lookup
+        cmd = matched_command.replace(" ", "_")
+
+        # slice off the remaining args of the original command
+        args = tokens[num_partial_cmd_tokens:]
+
+        return [cmd, args]
+    
+    return [ False, [] ]
+
 def parse_cmd(cmd_text, command_list):
 
     """
