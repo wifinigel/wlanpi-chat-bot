128c8c14ddf0cf267ee0d31e057dcd18443511f1
Nigel Bowden <wifinigel@gmail.com>
2021-02-23 22:36:14 +0000

    initial commit

---
+++ utils/constants.py
@@ -0,0 +1,9 @@
+"""
+Central store of all constants used across the chatbot code
+"""
+
+TELEGRAM_URL = "https://api.telegram.org"
+TELEGRAM_HOST = "api.telegram.org"
+TELEGRAM_PORT = 443
+
+CONFIG_FILE = "/opt/wlanpi-chat-bot/etc/config.json"
