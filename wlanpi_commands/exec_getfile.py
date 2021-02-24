07353b7acfbc9a59810f30e5f961e92327688cfd
Nigel Bowden <wifinigel@gmail.com>
2021-02-24 16:05:41 +0000

    Add getfile command

---
+++ wlanpi_commands/exec_getfile.py
@@ -0,0 +1,34 @@
+import os
+
+from wlanpi_commands.command import Command
+
+class ExecGetfile(Command):
+    
+    def __init__(self, telegram_object, conf_obj):
+        super().__init__(telegram_object, conf_obj)
+
+        self.telegram_object = telegram_object
+        self.command_name = "exec_getfile"
+       
+    def run(self, args_list):      
+
+        progress_msg = "Getting file..."
+        self.telegram_object.send_msg(progress_msg, self.telegram_object.chat_id)
+
+        filename = ''
+        if len(args_list) > 0:
+            filename = args_list[0]
+        else:
+             return"Please supply filename. Failed"
+
+        try:
+            with open(filename, 'rb') as document:
+                doc = document.read()
+        except IOError:
+            return("File access issue - please check path and name.")
+
+        if self.telegram_object.send_file(doc, self.telegram_object.chat_id):
+            return("File sent OK")
+        else:
+            return("File transfer failed")
+
