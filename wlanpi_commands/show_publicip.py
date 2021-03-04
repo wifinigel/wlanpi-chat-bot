700003495995d6a31657f2ace5b2bf3027d010a2
Nigel Bowden <wifinigel@gmail.com>
2021-03-04 07:22:39 +0000

    Add help msg

--- wlanpi_commands/show_publicip.py
+++ wlanpi_commands/show_publicip.py
@@ -6,8 +6,28 @@ class ShowPublicip(Command):
         super().__init__(telegram_object, conf_obj)
 
         self.command_name = "show_publicip"
+    
+    def help_message(self):
+        """
+        Return the help page for this command
+        """
+        long_msg = """Displays public IP that the probe is behind.       
+
+Args: None
+
+ Example: show publicip
+"""
+        short_msg = long_msg
+        return self._render_help(short_msg, long_msg)
        
-    def run(self, args_list):      
+    def run(self, args_list):
+        
+        # check if help rquired
+        if len(args_list) > 0:
+            if args_list[0] == "?":
+                    return self._render(self.help_message())
+            else:
+                return self._render("Unknown argument.")
 
         progress_msg = "Getting Public IP..."
         cmd_string = "/usr/share/fpms/BakeBit/Software/Python/scripts/networkinfo/publicip.sh"
