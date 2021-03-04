15cf11947746d21c25e8d02b5c151838441e6db0
Nigel Bowden <wifinigel@gmail.com>
2021-03-04 06:44:12 +0000

    Add help msg

--- wlanpi_commands/show_mode.py
+++ wlanpi_commands/show_mode.py
@@ -7,7 +7,28 @@ class ShowMode(Command):
 
         self.command_name = "show_mode"
     
+    def help_message(self):
+        """
+        Return the help page for this command
+        """
+        long_msg = """Displays current running mode of the probe (e.g. classic, wiperf, etc...).       
+
+Args: None
+
+ Example: show mode
+"""
+        short_msg = long_msg
+        return self._render_help(short_msg, long_msg)
+    
     def run(self, args_list):
+
+        # check if help rquired
+        if len(args_list) > 0:
+            if args_list[0] == "?":
+                    return self._render(self.help_message())
+            else:
+                return self._render("Unknown argument.")
+        
         STATUS_FILE="/etc/wlanpi-state"
         status = "WLAN Pi mode: {}".format(self._read_file( STATUS_FILE)[0])
         return self._render(status.strip())
