772538467b84ccca1158334d272153202c96469e
Nigel Bowden <wifinigel@gmail.com>
2021-03-04 06:42:32 +0000

    Add help msg

--- wlanpi_commands/show_lldp.py
+++ wlanpi_commands/show_lldp.py
@@ -7,7 +7,27 @@ class ShowLLDP(Command):
 
         self.command_name = "show_lldp"
     
+    def help_message(self):
+        """
+        Return the help page for this command
+        """
+        long_msg = """Displays LLDP info detected by the probe.       
+
+Args: None
+
+ Example: show lldp
+"""
+        short_msg = long_msg
+        return self._render_help(short_msg, long_msg)
+    
     def run(self, args_list):
+
+        # check if help rquired
+        if len(args_list) > 0:
+            if args_list[0] == "?":
+                    return self._render(self.help_message())
+            else:
+                return self._render("Unknown argument.")
         
         # read in CDP data is available
         cdp_data = self._read_file('/tmp/lldpneigh.txt')
